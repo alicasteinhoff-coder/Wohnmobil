8e559f5e3eb183a393ab11fec25fc3f223bc6c46
Developer <test@example.com>
2026-01-06 17:11:26 +0100

    Major website update: VanAway branding, Google Maps with 11 locations, vehicle details enhancement

---
+++ data_old.js
@@ -0,0 +1,452 @@
+window.vehicles = [
+    {
+        "id": 1,
+        "name": "Mercedes Sprinter Camper",
+        "category": "Kastenwagen",
+        "brand": "Mercedes-Benz",
+        "color": "Weiß",
+        "image": "images/mercedes_sprinter_camper.png",
+        "pricePerDay": 120,
+        "license": "Klasse B",
+        "transmission": "Automatik",
+        "power": 163,
+        "beds": 2,
+        "fuel": {
+            "type": "Diesel",
+            "consumption": 8.9,
+            "emissionClass": "Euro 6d",
+            "emissionSticker": "Grün"
+        },
+        "dimensions": {
+            "length": 5.93,
+            "width": 2.02,
+            "height": 2.75
+        },
+        "weights": {
+            "empty": 2800,
+            "max": 3500
+        },
+        "towing": {
+            "hasHitch": true,
+            "maxLoad": 2000
+        },
+        "kitchen": {
+            "stove": "2-Flammen Gasherd",
+            "fridge": "65L Kompressor-Kühlschrank",
+            "sink": "Edelstahl-Spüle",
+            "oven": false,
+            "microwave": false,
+            "dishwasher": false
+        },
+        "sanitary": {
+            "toilet": true,
+            "shower": true
+        },
+        "assistanceSystems": [
+            "Rückfahrkamera",
+            "Parkpilot vorne & hinten",
+            "Abstandstempomat (ACC)",
+            "Spurhalteassistent",
+            "Totwinkelassistent"
+        ],
+        "features": [
+            "Standheizung Webasto",
+            "Solaranlage 100W",
+            "Markise 3m",
+            "LED-Beleuchtung",
+            "Aufstelldach",
+            "Fahrradträger (2 Räder)"
+        ],
+        "availableLocations": [
+            "Berlin",
+            "München",
+            "Hamburg",
+            "Köln"
+        ]
+    },
+    {
+        "id": 2,
+        "name": "Mercedes Sprinter 4x4",
+        "category": "Kastenwagen",
+        "brand": "Mercedes-Benz",
+        "color": "Weiß",
+        "image": "images/mercedes_sprinter_4x4.png",
+        "pricePerDay": 145,
+        "license": "Klasse B",
+        "transmission": "Automatik",
+        "power": 190,
+        "beds": 2,
+        "fuel": {
+            "type": "Diesel",
+            "consumption": 9.8,
+            "emissionClass": "Euro 6d",
+            "emissionSticker": "Grün"
+        },
+        "dimensions": {
+            "length": 6.94,
+            "width": 2.02,
+            "height": 2.78
+        },
+        "weights": {
+            "empty": 2850,
+            "max": 3500
+        },
+        "towing": {
+            "hasHitch": true,
+            "maxLoad": 2500
+        },
+        "kitchen": {
+            "stove": "2-Flammen Gasherd",
+            "fridge": "70L Kompressor-Kühlschrank",
+            "sink": "Edelstahl-Spüle",
+            "oven": false,
+            "microwave": false,
+            "dishwasher": false
+        },
+        "sanitary": {
+            "toilet": true,
+            "shower": true
+        },
+        "assistanceSystems": [
+            "Rückfahrkamera",
+            "Abstandstempomat (ACC)",
+            "Notbremsassistent",
+            "Spurhalteassistent",
+            "Totwinkelassistent",
+            "Müdigkeitswarner"
+        ],
+        "features": [
+            "Allradantrieb 4x4",
+            "Solaranlage 200W",
+            "Außendusche",
+            "Standheizung Webasto",
+            "Offroad-Bereifung",
+            "LED-Beleuchtung",
+            "Aufstelldach"
+        ],
+        "availableLocations": [
+            "München",
+            "Frankfurt",
+            "Stuttgart"
+        ]
+    },
+    {
+        "id": 3,
+        "name": "VW California Ocean",
+        "category": "Kastenwagen",
+        "brand": "Volkswagen",
+        "color": "Grau Metallic",
+        "image": "images/vw_california_ocean.png",
+        "pricePerDay": 95,
+        "license": "Klasse B",
+        "transmission": "Automatik",
+        "power": 150,
+        "beds": 4,
+        "fuel": {
+            "type": "Diesel",
+            "consumption": 7.8,
+            "emissionClass": "Euro 6d",
+            "emissionSticker": "Grün"
+        },
+        "dimensions": {
+            "length": 4.90,
+            "width": 1.90,
+            "height": 1.99
+        },
+        "weights": {
+            "empty": 2470,
+            "max": 3080
+        },
+        "towing": {
+            "hasHitch": true,
+            "maxLoad": 2000
+        },
+        "kitchen": {
+            "stove": "2-Flammen Gasherd",
+            "fridge": "42L Kompressor-Kühlbox",
+            "sink": "Kompakt-Spüle",
+            "oven": false,
+            "microwave": false,
+            "dishwasher": false
+        },
+        "sanitary": {
+            "toilet": false,
+            "shower": true
+        },
+        "assistanceSystems": [
+            "Rückfahrkamera",
+            "Parkpilot vorne & hinten",
+            "Spurhalteassistent",
+            "Abstandstempomat (ACC)"
+        ],
+        "features": [
+            "Aufstelldach elektrisch",
+            "Markise",
+            "Campingtisch & Stühle",
+            "Standheizung",
+            "LED-Beleuchtung",
+            "Fahrradträger (2 Räder)"
+        ],
+        "availableLocations": [
+            "Berlin",
+            "Hamburg",
+            "München",
+            "Köln",
+            "Frankfurt",
+            "Leipzig"
+        ]
+    },
+    {
+        "id": 4,
+        "name": "VW Crafter Camper",
+        "category": "Kastenwagen",
+        "brand": "Volkswagen",
+        "color": "Weiß",
+        "image": "images/vw_crafter_camper.png",
+        "pricePerDay": 110,
+        "license": "Klasse B",
+        "transmission": "Automatik",
+        "power": 177,
+        "beds": 3,
+        "fuel": {
+            "type": "Diesel",
+            "consumption": 8.5,
+            "emissionClass": "Euro 6d",
+            "emissionSticker": "Grün"
+        },
+        "dimensions": {
+            "length": 6.83,
+            "width": 2.04,
+            "height": 2.80
+        },
+        "weights": {
+            "empty": 2750,
+            "max": 3500
+        },
+        "towing": {
+            "hasHitch": true,
+            "maxLoad": 2000
+        },
+        "kitchen": {
+            "stove": "2-Flammen Gasherd",
+            "fridge": "80L Kompressor-Kühlschrank",
+            "sink": "Edelstahl-Spüle",
+            "oven": false,
+            "microwave": false,
+            "dishwasher": false
+        },
+        "sanitary": {
+            "toilet": true,
+            "shower": true
+        },
+        "assistanceSystems": [
+            "Rückfahrkamera",
+            "Parkpilot hinten",
+            "Spurhalteassistent",
+            "Müdigkeitswarner"
+        ],
+        "features": [
+            "Standheizung",
+            "Solaranlage 150W",
+            "Markise 4m",
+            "Heckgarage",
+            "LED-Beleuchtung",
+            "Fahrradträger (3 Räder)"
+        ],
+        "availableLocations": [
+            "Hamburg",
+            "Berlin",
+            "Hannover"
+        ]
+    },
+    {
+        "id": 5,
+        "name": "Fiat Ducato Camper",
+        "category": "Kastenwagen",
+        "brand": "Fiat",
+        "color": "Weiß",
+        "image": "images/fiat_ducato_camper.png",
+        "pricePerDay": 105,
+        "license": "Klasse B",
+        "transmission": "Manuell",
+        "power": 140,
+        "beds": 3,
+        "fuel": {
+            "type": "Diesel",
+            "consumption": 8.2,
+            "emissionClass": "Euro 6d",
+            "emissionSticker": "Grün"
+        },
+        "dimensions": {
+            "length": 6.36,
+            "width": 2.05,
+            "height": 2.76
+        },
+        "weights": {
+            "empty": 2650,
+            "max": 3500
+        },
+        "towing": {
+            "hasHitch": true,
+            "maxLoad": 2000
+        },
+        "kitchen": {
+            "stove": "2-Flammen Gasherd",
+            "fridge": "75L Kompressor-Kühlschrank",
+            "sink": "Edelstahl-Spüle",
+            "oven": false,
+            "microwave": false,
+            "dishwasher": false
+        },
+        "sanitary": {
+            "toilet": true,
+            "shower": true
+        },
+        "assistanceSystems": [
+            "Rückfahrkamera",
+            "Parkpilot hinten",
+            "Spurhalteassistent"
+        ],
+        "features": [
+            "Standheizung Truma",
+            "Solaranlage 120W",
+            "Markise 3,5m",
+            "Heckgarage",
+            "LED-Beleuchtung",
+            "Fahrradträger (2 Räder)"
+        ],
+        "availableLocations": [
+            "Köln",
+            "Düsseldorf",
+            "Frankfurt",
+            "München"
+        ]
+    },
+    {
+        "id": 6,
+        "name": "Ford Transit Camper",
+        "category": "Kastenwagen",
+        "brand": "Ford",
+        "color": "Weiß",
+        "image": "images/ford_transit_camper.png",
+        "pricePerDay": 115,
+        "license": "Klasse B",
+        "transmission": "Automatik",
+        "power": 170,
+        "beds": 2,
+        "fuel": {
+            "type": "Diesel",
+            "consumption": 8.7,
+            "emissionClass": "Euro 6d",
+            "emissionSticker": "Grün"
+        },
+        "dimensions": {
+            "length": 6.70,
+            "width": 2.05,
+            "height": 2.72
+        },
+        "weights": {
+            "empty": 2720,
+            "max": 3500
+        },
+        "towing": {
+            "hasHitch": true,
+            "maxLoad": 2200
+        },
+        "kitchen": {
+            "stove": "2-Flammen Gasherd",
+            "fridge": "70L Kompressor-Kühlschrank",
+            "sink": "Edelstahl-Spüle",
+            "oven": false,
+            "microwave": false,
+            "dishwasher": false
+        },
+        "sanitary": {
+            "toilet": false,
+            "shower": true
+        },
+        "assistanceSystems": [
+            "Rückfahrkamera",
+            "Parkpilot vorne & hinten",
+            "Spurhalteassistent",
+            "Abstandstempomat (ACC)",
+            "Müdigkeitswarner"
+        ],
+        "features": [
+            "Standheizung Webasto",
+            "Solaranlage 150W",
+            "Markise 4m",
+            "Aufstelldach",
+            "LED-Beleuchtung",
+            "Fahrradträger (3 Räder)"
+        ],
+        "availableLocations": [
+            "München",
+            "Nürnberg",
+            "Stuttgart"
+        ]
+    },
+    {
+        "id": 7,
+        "name": "VW ID. Buzz Camper",
+        "category": "Kastenwagen",
+        "brand": "Volkswagen",
+        "color": "Weiß/Gelb",
+        "image": "images/vw_id_buzz_camper.png",
+        "pricePerDay": 135,
+        "license": "Klasse B",
+        "transmission": "Automatik",
+        "power": 204,
+        "beds": 2,
+        "fuel": {
+            "type": "Elektro",
+            "consumption": 21.0, // kWh/100km
+            "emissionClass": "Zero Emission",
+            "emissionSticker": "Grün"
+        },
+        "dimensions": {
+            "length": 4.71,
+            "width": 1.98,
+            "height": 1.93
+        },
+        "weights": {
+            "empty": 2400,
+            "max": 3000
+        },
+        "towing": {
+            "hasHitch": true,
+            "maxLoad": 1000
+        },
+        "kitchen": {
+            "stove": "Induktionskochfeld",
+            "fridge": "30L Kühlbox",
+            "sink": "Falt-Spüle",
+            "oven": false,
+            "microwave": false,
+            "dishwasher": false
+        },
+        "sanitary": {
+            "toilet": false,
+            "shower": false
+        },
+        "assistanceSystems": [
+            "Rückfahrkamera",
+            "Parkpilot vorne & hinten",
+            "Spurhalteassistent",
+            "Abstandstempomat (ACC)",
+            "Travel Assist"
+        ],
+        "features": [
+            "Vollelektrisch",
+            "Standklimatisierung",
+            "LED-Matrix-Scheinwerfer",
+            "Ambientebeleuchtung",
+            "Panoramadach"
+        ],
+        "availableLocations": [
+            "Berlin",
+            "München",
+            "Hamburg"
+        ]
+    }
+];
